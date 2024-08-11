3eb4f3e8f940957956a8d12e6e3b82761c30c1f9
Joseph Asare <asarekofi85@gmail.com>
2024-08-11 02:30:14 +0000

    commit

--- src/main/kotlin/Stock.kt
+++ src/main/kotlin/Stock.kt
@@ -7,6 +7,46 @@ class Stock(
     var amount: Double,
     var totalamount: Double
 ){
+    // Getter and Setter for stockid
+    fun retrieveStockId(): Int {
+        return stockid
+    }
+
+    fun updateStockId(id: Int) {
+        stockid = id
+    }
+
+    fun retrieveProductName(): String {
+        return productname
+    }
+
+    fun updateProductName(name: String) {
+        productname = name
+    }
+
+    fun retrieveQuantity(): Int {
+        return quantity
+    }
+
+    fun updateQuantity(qty: Int) {
+        quantity = qty
+    }
+
+    fun retrieveAmount(): Double {
+        return amount
+    }
+
+    fun updateAmount(amt: Double) {
+        amount = amt
+    }
+
+    fun retrieveTotalAmount(): Double {
+        return totalamount
+    }
+
+    fun updateTotalAmount(tmt: Double) {
+        totalamount = tmt
+    }
 
     override fun toString(): String {
         return "Stock ID: "+ stockid +"\tName: "+productname+"\tQty : "+quantity+"\tAmount : "+amount+"\tTotal : "+totalamount
--- src/main/kotlin/StockCRUD.kt
+++ src/main/kotlin/StockCRUD.kt
@@ -24,14 +24,66 @@ class StockCRUD {
         stockList.add(stockdata)
         println("Stock with ID: "+sid+" is Added Successfully")
     }
+
+
+
     fun deleteStock(){
 
+        print("Enter Stock ID to be deleted")
+        var del:Int = sc.nextInt()
+        var chk = Stock(0,"d",0,0.0,0.0)
+
+        for(sitem in stockList) {
+
+            chk=sitem
+            if(sitem.retrieveStockId() == del){
+
+                stockList.remove(sitem)
+                println("Stock ID : "+del+" is removed")
+            }
+
+        }
+        if(chk.retrieveStockId() != del){
+            println("Data be found")
+        }
     }
     fun displayStock(){
 
+        for(sitem in stockList){
+            println(sitem)
+        }
     }
     fun updateStock(){
 
+        print("Enter Stock Id to be Updated: ")
+        var sid: Int = sc.nextInt()
+        var chk = Stock(0,"d",0,0.0,0.0)
+
+        for(sitem in stockList){
+            if(sitem.retrieveStockId() == sid){
+
+                chk = sitem
+                stockList.remove(sitem)
+                print("Enter Updated Product Name: ")
+                var pname: String = sc.next().toString()
+                print("Enter Updated Quantity: ")
+                var uqty:Int = sc.nextInt()
+                print("Enter Updated Amount: ")
+                var uamt:Double = sc.nextDouble()
+
+                var utotalamt:Double = uamt*uqty
+
+                var update = Stock(sid,pname,uqty,uamt,utotalamt)
+
+                stockList.add(update)
+                println("Stock ID: "+sid+" is successfully Updated")
+
+
+            }
+        }
+        if(chk.retrieveStockId() != sid){
+            println("Data Not Found")
+        }
     }
 
 
